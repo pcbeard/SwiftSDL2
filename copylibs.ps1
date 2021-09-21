2c6985eea82ae115c97ad8252d978373690b24bc
pcbeard <pcbeard@mac.com>
2021-09-20 18:20:35 -0700

    Add zlib

--- copylibs.ps1
+++ copylibs.ps1
@@ -3,9 +3,12 @@ $libdir = (pkgconf --variable libdir sdl2).Trim()
 foreach ($config in "debug", "release") {
     $path = ".build/$config"
     if (Test-Path -Path $path) {
-        foreach($lib in "SDL2", "SDL2_gfx", "SDL2_image", "libpng16") {
+        foreach($lib in "SDL2", "SDL2_gfx", "SDL2_image", "libpng16", "zlib1") {
             Copy-Item ($bindir + "/$lib.dll") $path
-            Copy-Item ($libdir + "/$lib.lib") $path
+            $libpath = ($libdir + "/$lib.lib")
+            if (Test-Path -Path $libpath) {
+                Copy-Item $libpath $path
+            }
         }
     }
 }
