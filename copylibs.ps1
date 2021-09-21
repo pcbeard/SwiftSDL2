1744cb3fa7467b7261cd55cc711867363e7e93b3
pcbeard <pcbeard@mac.com>
2021-09-20 18:04:50 -0700

    Also install libpng16

--- copylibs.ps1
+++ copylibs.ps1
@@ -3,7 +3,7 @@ $libdir = (pkgconf --variable libdir sdl2).Trim()
 foreach ($config in "debug", "release") {
     $path = ".build/$config"
     if (Test-Path -Path $path) {
-        foreach($lib in "SDL2", "SDL2_gfx", "SDL2_image") {
+        foreach($lib in "SDL2", "SDL2_gfx", "SDL2_image", "libpng16") {
             Copy-Item ($bindir + "/$lib.dll") $path
             Copy-Item ($libdir + "/$lib.lib") $path
         }
